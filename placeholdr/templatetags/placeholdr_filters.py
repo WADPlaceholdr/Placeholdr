0ee9f2be832fd8b69a34436c001dfea91099db89
guillaumedsde <guillaumedsde@gmail.com>
2018-03-14 23:56:59 +0000

    stars on place cards now work

--- placeholdr/templatetags/placeholdr_filters.py
+++ placeholdr/templatetags/placeholdr_filters.py
@@ -4,6 +4,9 @@ register = template.Library()
 
 @register.filter(name='times')
 def times(number):
+    print("NUMBER: "+ number)
+    if not number:
+        return []
     return range(int(number))
 
 @register.filter
