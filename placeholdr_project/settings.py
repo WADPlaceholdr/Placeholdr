0863e785b3c1fee7b8cd6e31c4027553e97f7c23
guillaumedsde <guillaumedsde@gmail.com>
2018-03-14 20:03:55 +0000

    deployment ready

--- placeholdr_project/settings.py
+++ placeholdr_project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-
+import importlib
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -151,3 +151,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [STATIC_DIR, ]
+
+deployment_variables = importlib.util.find_spec("spam")
+# If deployment variables are present
+if deployment_variables is not None:
+    # import them
+    from deployment_variables.py import *
