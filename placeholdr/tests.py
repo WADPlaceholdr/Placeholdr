89fe5de1b7394a68b893bbd327e32a1f1827ecd0
Leonore Papaloizos <2264897V@student.gla.ac.uk>
2018-03-15 13:09:59 +0000

    work on populate script for more data

--- placeholdr/tests.py
+++ placeholdr/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
 
-# Create your tests here.
+# Index contains appropriate ratings
+# Empty user page (e.g. favourite trip) displays appropriate message
+# Check places and trips have maps displayed
+# Test submitting a place works
+# Test submitting a trip works
+# Test submitting a review works
+# Test population script works
+# Test place for slug field
+# Test trip for slug field
+# Test user url works
+# Test content of index before/after populating
+# Test index displays no content message
+# Test place page displays related trips
+# Test place page displays reviews
+# Test trip page displays reviews
+# Test place that does not exist displays appropriate message
+# Trip that does not exist displays appropriate message
+# User that does not exist displays appropriate message
+# Test forms are displayed correctly
+# Test displayed links when logged in/logged out
+# Test models
+# Test upload images works
+# Test visits count works
+# Test user image shows up top right
+# Check featured trips are not just random
+# Check places and trips display tags or no tags yet
+# Check search looks through all models
