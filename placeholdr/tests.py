936bc8a1c214dc969dd922cd70cf19091c201fc8
Leonore Papaloizos <2264897V@student.gla.ac.uk>
2018-03-19 16:11:52 +0000

    more unit tests

--- placeholdr/tests.py
+++ placeholdr/tests.py
@@ -17,7 +17,8 @@ def create_user():
     user.save()
 
     # Link it to a user profile
-    user_profile = UserProfile.objects.get_or_create(user=user, bio="I am just a test user", livesIn="Command Line", rep=200)[0]
+    user_profile = \
+        UserProfile.objects.get_or_create(user=user, bio="I am just a test user", livesIn="Command Line", rep=200)[0]
     user_profile.save()
 
     return user_profile
@@ -26,7 +27,7 @@ def create_user():
 def create_place(user):
     i = user.user.id
     place = Place(name="Place " + str(i), lat=i * 78.2357, long=i * 15.4913,
-                      desc="I'm just a place in location " + str(i), userId=user)
+                  desc="I'm just a place in location " + str(i), userId=user)
     place.save()
 
     return place
@@ -38,16 +39,49 @@ def create_place_review(user, place):
     return review
 
 
-def create_trip(user, places):
-    trip = Trip(userId=user, name="Trip " + str(user.user.id), desc="Just a trip")
-    trip.save()
+def create_trip(trip, user, places):
     trip_nodes = []
+
     for i in range(0, len(places)):
         node = TripNode(placeId=places[i].id, tripId=trip.id, userId=user.user.id)
         node.save()
         trip_nodes.append(node)
 
     return trip_nodes
+
+
+def create_multiple_places(user):
+    places = []
+
+    # Create place 1 through 4
+    for i in range(1, 5):
+        place = Place(name="Place " + str(i), lat=i * 78.2357, long=i * 15.4913,
+                      desc="I'm just a place in location " + str(i), userId=user)
+        place.save()
+        places.append(place)
+    return places
+
+
+def create_multiple_trips(user):
+    trips = []
+
+    # Create trip 1 through 4
+    for i in range(1, 5):
+        trip = Trip(userId=user, name="Trip " + str(i), desc="I'm just trip number " + str(i))
+        trip.save()
+        trips.append(trip)
+    return trips
+
+
+def create_top_users():
+    users = []
+    for i in range(1, 11):
+        user = User(username="user" + str(i), password="test")
+        user.save()
+        up = UserProfile(user=user, rep=i)
+        up.save()
+        users.append(up)
+    return users
 
 
 class IndexTests(TestCase):
@@ -63,6 +97,40 @@ class IndexTests(TestCase):
         response = self.client.get(reverse('index'))
         self.assertIn('Top Users'.lower(), response.content.decode('ascii').lower())
 
+    # Test content of index before populating
+    def test_index_empty_content(self):
+        # Access with empty database
+        response = self.client.get(reverse('index'))
+
+        # Context dictionary is then empty
+        self.assertCountEqual(response.context['places'], [])
+        self.assertCountEqual(response.context['userProfiles'], [])
+        self.assertCountEqual(response.context['trips'], [])
+
+    # With 4 places, make sure they are all displayed in the index (featured = random for now)
+    def test_index_displays_four_featured_places(self):
+        places = create_multiple_places(create_user())
+        response = self.client.get(reverse('index'))
+
+        for i in range(1, 5):
+            self.assertIn("Place " + str(i), response.content.decode('ascii'))
+
+    # With 4 trips, make sure they are all displayed in the index (featured = random for now)
+    def test_index_displays_four_featured_trips(self):
+        trips = create_multiple_trips(create_user())
+        response = self.client.get(reverse('index'))
+
+        for i in range(1, 5):
+            self.assertIn("Trip " + str(i), response.content.decode('ascii'))
+
+    def test_index_displays_six_top_users(self):
+        users = create_top_users()
+        response = self.client.get(reverse('index'))
+
+        for i in range(10, 5, -1):
+            user = users[i - 1]
+            self.assertIn("user" + str(i), response.content.decode('ascii'))
+
 
 class ModelTests(TestCase):
     def setUp(self):
@@ -71,36 +139,32 @@ class ModelTests(TestCase):
         self.place_review = create_place_review(self.user, self.place)
 
     def test_create_a_new_user(self):
-
         # Check that User is in database
         users_in_database = UserProfile.objects.all()
         self.assertEquals(len(users_in_database), 1)
         self.assertEquals(users_in_database[0], self.user)
 
     def test_create_a_new_place(self):
-
         # Check that Place is in database
         places_in_database = Place.objects.all()
         self.assertEquals(len(places_in_database), 1)
         self.assertEquals(places_in_database[0], self.place)
 
     def test_create_new_place_review(self):
-
         # Check that PlaceReview is in database
         place_reviews_in_database = PlaceReview.objects.all()
         self.assertEquals(len(place_reviews_in_database), 1)
         self.assertEquals(place_reviews_in_database[0], self.place_review)
-
-class PopulateScript(TestCase):
 
     def test_population_script_changes(self):
         # populate
         populate_placeholdr.populate()
 
         # Check for one user
-        # u = UserProfile.objects.get(self="michael")
-        # self.assertEquals(u.rep, 2360)
-        # self.assertEquals(u.livesIn, 'London')
+        u = User.objects.get(username="michael")
+        up = UserProfile.objects.get(user=u)
+        self.assertEquals(up.rep, 2360)
+        self.assertEquals(up.livesIn, 'London')
 
         # Check for one place
         p = Place.objects.get(name='Papercup Glasgow')
@@ -113,6 +177,8 @@ class PopulateScript(TestCase):
 
 
 class UrlTests(TestCase):
+    # Error messages
+    # Test place, trip contains add review when appropriate
 
     def test_place_contains_slug_field(self):
         place = Place(name="Mount Everest", userId=create_user())
@@ -126,48 +192,75 @@ class UrlTests(TestCase):
         # Check slug was generated
         self.assertEquals(trip.slug, "roadtripping-through-europe")
 
+    # Make sure you can access a user profile
+    def test_user_url(self):
+        u = create_user()
+        response = self.client.get(reverse('show_user', args=[u.user.username]))
+        self.assertIn(u.bio, response.content.decode('ascii'))
+
+    def test_access_place_that_does_not_exist(self):
+        response = self.client.get(reverse('show_place', args=['neverland']))
+
+        # Check that it has a response as status code OK is 200
+        # self.assertEquals(response.status_code, 200)
+
+        # Check the rendered page is not empty = customised
+        self.assertNotEquals(response.content.decode('ascii'), '')
+
+    def test_access_trip_that_does_not_exist(self):
+        response = self.client.get(reverse('show_trip', args=['perfect-trip']))
+
+        # Check that it has a response as status code OK is 200
+        # self.assertEquals(response.status_code, 200)
+
+        # Check the rendered page is not empty = customised
+        self.assertNotEquals(response.content.decode('ascii'), '')
 
 
 class ContentTests(TestCase):
+    # TODO
     # Index contains appropriate ratings
-    # Empty user page (e.g. favourite trip) displays appropriate message
     # Check places and trips have maps displayed
-    # Test content of index before/after populating
-    # Test index displays no content message
+    # Check new places
+    # Check top places
+    # Check new trips
+    # Check top trips
 
-    # Test place that does not exist displays appropriate message
-    # Trip that does not exist displays appropriate message
-    # User that does not exist displays appropriate message
-    None
+    def trip_page_displays_places(self):
+        # Create places, user, trip
+        user = create_user()
+        places = create_multiple_places(user)
+        trip = Trip.objects.get_or_create(userId=user, name="Trip " + str(user.user.id), desc="Just a trip")[0]
+        trip_nodes = create_trip(trip, user, places)
 
+        for node in trip_nodes:
+            response = self.client.get(reverse('show_trip', args=[trip.slug]))
+            self.assertIn(Place.object.get(placeId=node.placeId), response.content.decode('ascii'))
 
-class FeaturesTests(TestCase):
-    # Test visits count works
-    # Check search looks through all models
-    None
-
-
-class PlaceTests(TestCase):
-    # Check places and trips display tags or no tags yet
-    # Test place page displays related trips
-    # Test place page displays reviews
-    None
-
-
-class TripTests(TestCase):
-    # Check places and trips display tags or no tags yet
-    # Test trip page displays reviews
-    # Check featured trips are not just random
-    None
 
 
 class FormTests(TestCase):
-    # Test upload images works
-    # Test forms are displayed correctly
+    # TODO
+    # Registration form
+    def setUp(self):
+        self.user = create_user()
+
+    def test_registration_form_is_displayed_correctly(self):
+        None
+
+    def test_registration_form_is_valid(self):
+        None
+
+    # TODO
+    # Submit place form
+    # Submit trip form
+    # Review form
+
     None
 
 
 class UserTests(TestCase):
+    # TODO
     # Test displayed links when logged in/logged out
     # Test user image shows up top right
     None
